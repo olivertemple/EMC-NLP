9ac15ce5a6e3e070e4a8d0c1b923f8678ebc3b11
olivertemple <70803502+olivertemple@users.noreply.github.com>
2021-01-15 16:09:44 +0000

    added stopping words.py for the part1.3 and a .gitignore

---
+++ stopping words.py
@@ -0,0 +1,15 @@
+import math
+
+def removeStopWordPercent(N,k):
+    sumlower=0
+    sumupper=0
+    for n in range(1,k+1):
+        sumlower+= 1/(n*math.log10(N)+n)
+        sumupper+=1/(n*math.log10(N))
+    return (sumlower,sumupper)
+
+N = 10e4
+k=10
+
+sumlower,sumupper = removeStopWordPercent(N,k)
+print(str((sumlower/N)*100)+"% <= freq <= "+str((sumupper/N)*100))
