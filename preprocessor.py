e81237d61dec5b0f5a251cf77eaa36b754e309f2
olivertemple <70803502+olivertemple@users.noreply.github.com>
2021-01-16 12:58:46 +0000

    added preprocessor.py

---
+++ preprocessor.py
@@ -0,0 +1,32 @@
+import string
+from nltk import download
+import nltk
+from nltk.corpus import stopwords
+
+#download('stopwords')
+#nltk.download('punkt')
+stop_words = stopwords.words('english')
+
+def basic_tokenizer(sentence):
+    string2 = sentence.lower()
+    newstring = ''
+    for char in string2:
+        if char not in punctuation:
+            newstring += char
+    stingOfWord = newstring.split(' ')
+    return(stingOfWord)
+
+def remove_stopwords(list, stop_words):
+    output = []
+    for item in list:
+        if item not in stop_words:
+            output.append(item)
+    return output
+
+def preprocess(sentence, stop_words):
+    sentence = basic_tokenizer(sentence)
+    clean_sentence = remove_stopwords(sentence, stop_words)
+
+    return clean_sentence
+
+print(preprocess("This is a test sentence for the preprocessor", stop_words))
